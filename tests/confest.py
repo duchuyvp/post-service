7045cf7f9f9c3a50cdd3aa91de8acec50938aefb
Nguyễn Đức Huy <duchuvp@Lovegood.local>
2024-03-15 23:53:42 +0700

    Add dataSources.xml and update sql_session_factory

--- tests/confest.py
+++ tests/confest.py
@@ -19,7 +19,7 @@ pytest.register_assert_rewrite("tests.e2e.api_client")
 
 @pytest.fixture
 def sql_session_factory():
-    # engine = sa.create_engine("postgresql://postgres:postgres@localhost:5432/testdb")
+    # engine = sa.create_engine("postgresql://postgres:postgres@localhost:5432/db")
     engine = sa.create_engine("sqlite:///:memory:")
     orm.mapper_registry.metadata.create_all(engine)
     yield sessionmaker(bind=engine)
