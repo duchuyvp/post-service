4a98f86b7d371c11839ea0811515ba7771d9aba9
Nguyễn Đức Huy <Luna@Lovegood.local>
2024-05-22 18:43:23 +0700

    --

--- tests/confest.py
+++ tests/confest.py
@@ -15,7 +15,7 @@ pytest.register_assert_rewrite("tests.e2e.api_client")
 
 @pytest.fixture(scope="session")
 def sql_session_factory():
-    engine = sa.create_engine("postgresql://postgres:postgres@localhost:5432/db")
+    engine = sa.create_engine("postgresql://postgres:postgres@localhost:5432/test_db")
     # engine = sa.create_engine("sqlite:///:memory:")
     orm.metadata.create_all(engine)
     yield sessionmaker(bind=engine)
